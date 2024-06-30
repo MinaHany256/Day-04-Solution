d133e39cdfb42a68a44481504ec90918fc8cf3e9
MinaHany256 <minahanyfoad@gmail.com>
2024-06-30 15:07:38 +0300

    Part 01 Functions (Prototype, Calling)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,44 @@
 ﻿namespace Demo
 {
-    internal class Program
+    class Program
     {
+
+        #region Function Prototype and Calling
+
+        ///static void PrintShapes()
+        ///{
+        ///    for (int i = 1; i <= 10; i++)
+        ///    {
+        ///        Console.WriteLine("%_%");
+        ///    }
+        ///}
+
+        ///static void PrintShapes(int Count = 10, string shape = "@_@")
+        ///{
+        ///    for (int i = 1; i <= Count; i++)
+        ///    {
+        ///        Console.WriteLine($"{shape}");
+        ///    }
+        ///} 
+        ///
+
+        #endregion
+
+
+
         static void Main()
         {
-            Console.WriteLine("Hello Demo");
+
+            #region Function Prototype and Calling
+            //PrintShapes(5,"#_#");
+            //PrintShapes(shape:"*_*",Count:5);
+            //PrintShapes(5);
+
+            //Console.WriteLine("Hi\nmina\thany");
+            //Console.WriteLine(@"Hi\nmina\thany"); 
+            #endregion
+
+
         }
     }
 }
